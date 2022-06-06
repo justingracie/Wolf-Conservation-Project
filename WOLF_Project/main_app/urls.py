d82677b16a0e3bffe2c58498bb044a2b31d44716
justingracie <j.m.gracie@gmail.com>
2022-06-06 12:51:12 -0600

    added first templates and tested

--- WOLF_Project/main_app/urls.py
+++ WOLF_Project/main_app/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
+    path('conservation/', views.Conservation.as_view(), name="conservation"),
+    path('yourstory/', views.Yourstory.as_view(), name="yourstory"),
 
 ]
--- WOLF_Project/main_app/views.py
+++ WOLF_Project/main_app/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
-class Home(View):
-    def get(self, request):
-        return HttpResponse('Wolf Project Home')
+class Home(TemplateView):
+    template_name = "home.html"
+
+class Conservation(TemplateView):
+    template_name = 'conservation.html'
+    
+
+class Yourstory(TemplateView):
+    template_name = "yourstory.html"
+
