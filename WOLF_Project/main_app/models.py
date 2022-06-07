9d9321e2dbdb89bf2b78332a2df2766276010c87
justingracie <j.m.gracie@gmail.com>
2022-06-07 12:45:41 -0600

    updated data for pack detail pages

--- WOLF_Project/main_app/models.py
+++ WOLF_Project/main_app/models.py
@@ -3,8 +3,9 @@ from django.forms import CharField
 
 # Create your models here.
 class Video(models.Model):
-    caption=models.CharField(max_length=125)
-    video=models.FileField(upload_to="video/%y")
+    # video=models.FileField(upload_to="video/%y", default='no video')
+    link=models.CharField(max_length=400, default='no link')
+    title=models.CharField(max_length=125, default='no title')
 
     def __str__(self):
         return self.title
@@ -16,7 +17,7 @@ class Pack(models.Model):
     adults = models.CharField(max_length=25, default=0)
     pups = models.CharField(max_length=25, default=0)
     total = models.CharField(max_length=25, default=0)
-    history = models.CharField(max_length=100, default='No info available')
+    history = models.CharField(max_length=1200, default='No info available')
     img = models.CharField(max_length=500, default='no image')
 
     def __str__(self):
